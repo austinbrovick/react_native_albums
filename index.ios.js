1b1adf6cafeb8d36dc4533a97c1bc159fa7a23ec
Austin Brovick <austinbrovick@gmail.com>
2017-07-09 19:51:40 +0200

    got some text to show up

---
+++ index.ios.js
@@ -0,0 +1,21 @@
+// Index.ios.js
+
+// Import a library to help create a component
+import React from 'react'; // imports React library
+import { Text, AppRegistry } from 'react-native'; // imports ReactNative library
+
+
+
+// Create a component
+const App = () => (
+  <Text>Some Text</Text>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App); // albums because that is the name of our project
+
+
+
+
+// React vs ReactNative
+// ES6 == new version of javascript
