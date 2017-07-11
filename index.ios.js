232a9eeba5be2a57b766fa1c0ec52da4f3d02b02
Austin Brovick <austinbrovick@gmail.com>
2017-07-11 21:07:40 +0200

    Added AlbumList class and style to header

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,16 @@
 
 // Import a library to help create a component
 import React from 'react'; // imports React library
-import { AppRegistry } from 'react-native'; // imports ReactNative library
-import Header from './src/components/header';
-
+import { AppRegistry, View } from 'react-native'; // imports ReactNative library
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-  <Header />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
@@ -16,3 +19,6 @@ AppRegistry.registerComponent('albums', () => App);
 // albums because that is the name of our project
 // React vs ReactNative
 // ES6 == new version of javascript
+
+
+// whenever you pass data from parent to child, we use props
