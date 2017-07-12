3034661920a3c7632147acb138d18ca9a9cba202
Austin Brovick <austinbrovick@gmail.com>
2017-07-12 21:21:50 +0200

    Added Card and AlbumDetail

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 
 class AlbumList extends Component {
+  state = { albums: [] };
+  // called the instance the app loads
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
+
   render() {
+    console.log(this.state);
+
     return (
       <View>
-        <Text>Album List!!</Text>
+        { this.renderAlbums() }
       </View>
     );
   }
@@ -14,3 +33,13 @@ class AlbumList extends Component {
 
 
 export default AlbumList;
+
+
+// lifecycle methods
+// state is how react handles changes to the app
+  // e.g. we have data or we don't
+// setState already exists in all of our class based components
+// this.state = {} never do this!! unless you're initializing
+
+// props is for parent to child communication
+// state is for components internal recording keeping
