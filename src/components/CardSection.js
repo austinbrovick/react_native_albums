35da26feedd048a69c60abc4a0f64a00932529a5
Austin Brovick <austinbrovick@gmail.com>
2017-07-13 21:29:39 +0200

    Added Button and links

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,2 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
+
+// class or functional component?
+
+const CardSection = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative',
+  }
+};
+
+
+export default CardSection;
